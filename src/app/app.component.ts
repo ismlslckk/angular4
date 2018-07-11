a1bcd9b334d72db14cd3529fc4345b54d5ec4cad
ismail selçuk <ismlslck@gmail.com>
2018-07-11 21:28:16 +0300

    11 temmuz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'deneme projesi';
 }
+
+/*
+-selector:html içerisinde hangi isimle çağırıcağımızı belirtiyoruz.
+-templateUrl:bu componente ait html dosyasını belirtiyoruz.
+-styleUrls:html dosyasının kullanacağı css dosyalarını içerir.
+./ eğer aynı klasördeki yolu göstericeksek bu şekilde tanımlama yapmak gerekmektedir.
+-ng g component product -> yeni component oluşturur.
+-ts:type script,aslında class'tır.
+-.spec.ts:unit test oldugunu belirtir.
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { CartComponent } from './cart/cart.component';
+import { AccountComponent } from './account/account.component';
+import { ShippingDetailComponent } from './shipping-detail/shipping-detail.component';
+import { CartSummaryComponent } from './cart/cart-summary/cart-summary.component';
+import { LoggedComponent } from './account/logged/logged.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    CartComponent,
+    AccountComponent,
+    ShippingDetailComponent,
+    CartSummaryComponent,
+    LoggedComponent,
+    NotFoundComponent
   ],
   imports: [
+    BrowserModule
+  ],
+  exports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+bootstrap:bu modülün başlangıç yani ana componentini belirtir.
+  export diyerek bu classın public oldugunu belirtiyoruz.*/
