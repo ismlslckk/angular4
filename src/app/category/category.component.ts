013e18f34a88717110c87a8d219970a547c7c676
ismail selçuk <ismlslck@gmail.com>
2018-07-19 23:11:56 +0300

    ders 20 bitti.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Category} from './category'
+import {CategoryService} from './category.service'
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  providers:[CategoryService]
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService:CategoryService) { }
 
+  categories:Category[];
+  getCategories(){
+      this.categoryService.getCategories().subscribe(c=>this.categories=c);
+  }
   ngOnInit() {
+    this.getCategories();
   }
 
 }
