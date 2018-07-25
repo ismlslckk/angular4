4f68fb32baea2872006cb3923fbd2be6d49ef87d
ismail selçuk <ismlslck@gmail.com>
2018-07-25 22:55:38 +0300

    ders 34 bitti

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms'
+import {AccountService} from './account.service'
+import {Router,ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'app-account',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  returnUrl:string;
+  message:string;
+
+  constructor(private accountService:AccountService,
+              private activatedRoute:ActivatedRoute,
+              private router:Router) { }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe(params=>{
+      this.returnUrl=params["returnUrl"] || "products";
+    })
+  }
+
+  loginUser(form:NgForm){
+    this.accountService.login(form.value.user,form.value.password).subscribe(t=>{
+        if(t){
+            this.router.navigateByUrl(this.returnUrl);
+        }else{
+          this.message="Username or Password is incorrect";
+        }
+      }
+  );
   }
 
 }
--- src/app/account/logged/logged.component.ts
+++ src/app/account/logged/logged.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { AccountService } from 'src/app/account/account.service'
+import { Router } from '@angular/router'
+
 
 @Component({
   selector: 'app-logged',
   templateUrl: './logged.component.html',
   styleUrls: ['./logged.component.css']
 })
-export class LoggedComponent implements OnInit {
+export class LoggedComponent implements DoCheck {
 
-  constructor() { }
+  isLogged:boolean=false;
+  constructor(private accountService: AccountService,
+    private router: Router) { }
 
-  ngOnInit() {
+  ngDoCheck(): void {
+    this.isLogged=this.accountService.isLoggedIn();
+  }
+
+  logOut(){
+    this.accountService.logOut();
+    this.router.navigate(["account"]);
   }
 
 }
